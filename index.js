02789fb9bb651bf77fbcf864481d71b26468dab3
Josh Kramer <joshtkramer@yahoo.com>
2017-10-23 23:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,5 @@ function kittens(){
 
 function removeLastKitten(){
   var kittens = ["Milo", "Otis", "Garfield"];
-  var sliced = kittens.slice(3);
-  return kittens.slice(3);
+    return kittens.slice(3);
 }
